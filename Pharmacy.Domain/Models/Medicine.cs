93c9455def69117b5ddbb5413c4d4a1210da64b2
Ivan Zelenyy <m307924@alunos.ipb.pt>
2021-11-25 13:51:57 +0000

    Create Domain layout Create class "Medicine"

---
+++ Pharmacy.Domain/Models/Medicine.cs
@@ -0,0 +1,32 @@
+﻿using Pharmacy.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Domain.Models
+{
+    public class Medicine: Entity
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string HowToUse { get; set; }
+
+        public Medicine(
+                        string category, string name, string brand,
+                        float price, string description, string ingredients, string howToUse
+                        )
+        {
+            Category = category;
+            Name = name;
+            Brand = brand;
+            Price = price;
+            Description = description;
+            Ingredients = ingredients;
+            HowToUse = howToUse;
+        }
+    }
+}
