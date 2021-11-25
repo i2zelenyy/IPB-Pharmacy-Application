9eb7c763410373a61ae5b5b95b0d7261c3627818
Ivan Zelenyy <m307924@alunos.ipb.pt>
2021-11-25 14:11:06 +0000

    Create class "Stores"

--- Pharmacy.Domain/Models/Stores.cs
+++ Pharmacy.Domain/Models/Stores.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Pharmacy.Domain.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Pharmacy.Domain.Models
 {
-    class Stores
+    public class Stores: Entity
     {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Telephone { get; set; }
+        public string OpeningHours { get; set; }
 
+        public Stores(
+                      string name, string street, string city,
+                      string country, string telephone, string openingHours
+                      )
+        {
+            Name = name;
+            Street = street;
+            City = city;
+            Country = country;
+            Telephone = telephone;
+            OpeningHours = openingHours;
+        }
     }
 }
