d9fa5da63a5913203dd1a6c77bba877f38912073
Ivan Zelenyy <m307924@alunos.ipb.pt>
2021-11-30 00:57:03 +0000

    Create basic Home page

--- Pharmacy.UWP/App.xaml.cs
+++ Pharmacy.UWP/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pharmacy.UWP.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,45 +18,31 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Pharmacy.UWP
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
+
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: Load state from previously suspended application
+                    
                 }
 
-                // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
@@ -63,12 +50,9 @@ namespace Pharmacy.UWP
             {
                 if (rootFrame.Content == null)
                 {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    // Change to --> typeof(SignInPage)
+                    rootFrame.Navigate(typeof(HomePage), e.Arguments);
                 }
-                // Ensure the current window is active
                 Window.Current.Activate();
             }
         }
