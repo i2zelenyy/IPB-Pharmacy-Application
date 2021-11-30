b3c54c5e7430cba795cfa30ec048599dd6e918be
Ivan Zelenyy <m307924@alunos.ipb.pt>
2021-11-30 12:53:00 +0000

    Menu is compressed when page starts Add icon

--- Pharmacy.UWP/App.xaml.cs
+++ Pharmacy.UWP/App.xaml.cs
@@ -26,8 +26,6 @@ namespace Pharmacy.UWP
             this.Suspending += OnSuspending;
         }
 
-
-        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
             Frame rootFrame = Window.Current.Content as Frame;
@@ -57,27 +55,14 @@ namespace Pharmacy.UWP
             }
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
     }
