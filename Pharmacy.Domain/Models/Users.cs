0867356222afb29dbb9dd53643c8efd3a565a696
Gersica <a37785@alunos.ipb.pt>
2021-12-13 17:32:10 +0000

    Class User

---
+++ Pharmacy.Domain/Models/Users.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Domain.Models
+{
+    public class Users
+    { public string FirstName { get; set; }
+
+     public string LastName { get; set; }
+
+     public DateTime DateOfBirth { get; set; }
+
+     public string Username { get; set; }
+
+     public string Telephone { get; set; }
+
+     public string Street { get; set; }
+
+     public string Password { get; set; }
+
+     public string Email { get; set; }
+
+     public bool IsAdmin { get; set; }
+
+        public Users (string firstName, string lastName,DateTime dateOfBirth,
+            string username, string telephone, string street, string password, string email,
+            bool isAdmin)
+
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Username = username;
+            Telephone = telephone;
+            Street = street;
+            Password = password;
+            Email = email;
+            IsAdmin = isAdmin;
+        }
+
+
+
+
+    }
+}
