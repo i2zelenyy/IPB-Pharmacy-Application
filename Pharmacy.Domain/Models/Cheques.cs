f0ff030a1c9a7f57ad16ce285ee5cf0a371ec8a7
Gersica <a37785@alunos.ipb.pt>
2021-12-20 19:10:18 +0000

    Creation of class Cheques

---
+++ Pharmacy.Domain/Models/Cheques.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Domain.Models
+{
+    public class Cheques
+    {
+        public DateTime Date;
+        public DateTime Time;
+        public float TotalPrice;
+        public int StoreID;
+        public int BasketID;
+
+        public Cheques(DateTime date, DateTime time, 
+            float totalPrice, int storeID, int basketID)
+        {
+            Date = date;
+            Time = time;
+            TotalPrice = totalPrice;
+            StoreID = storeID;
+            BasketID = basketID;
+        }
+
+        public Baskets Baskets { get; set; }  // 1-many relationship
+        public Stores Stores { get; set; }  // 1-many relationship
+
+    }
+}
