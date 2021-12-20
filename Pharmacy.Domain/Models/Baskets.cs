1b8ccb4c6d7a6b5d292192b46c53617a373d4ccd
Gersica <a37785@alunos.ipb.pt>
2021-12-20 18:49:42 +0000

    Creation of Basket class

---
+++ Pharmacy.Domain/Models/Baskets.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Domain.Models
+{
+   public class Baskets
+    {
+        public int Quantity { get; set; }
+
+        public int MedicineID { get; set; }
+
+        public int UserID { get; set; }
+
+
+
+
+        public Users Users { get; set; }  // 1-many relationship
+
+        public Medicine Medicine { get; set; } //1-many relationship
+
+      
+        
+        public Baskets(int quantity, int medicineID, int userID)
+        {
+            Quantity = quantity;
+            MedicineID = medicineID;
+            UserID = userID;
+        }
+    }
+}
