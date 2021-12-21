8d21c43558eedb4365f104a96f926ee36ed683ec
Gersica <a37785@alunos.ipb.pt>
2021-12-21 15:46:21 +0000

    Creation of Infrastructure Baskets Repository

---
+++ Pharmacy.Infrastructure/Repositories/BasketsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Domain.Models;
+using Pharmacy.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Infrastructure.Repositories
+{
+    public class BasketsRepository : Repository<Baskets>, IBasketsRepository
+    {
+        public BasketsRepository(PharmacyDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        
+    }
+    }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
