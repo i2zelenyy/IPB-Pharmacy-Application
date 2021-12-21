e85a97c8bcd881e65d3d814f319764708e209b87
Gersica <a37785@alunos.ipb.pt>
2021-12-21 16:31:57 +0000

    Create Cheques Repository (Infrastructure)

---
+++ Pharmacy.Infrastructure/Repositories/ChequesRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Domain.Models;
+using Pharmacy.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Infrastructure.Repositories
+{
+    public class ChequesRepository : Repository<Cheques>, IChequesRepository
+    {
+        public ChequesRepository(PharmacyDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+
+    }
+}
