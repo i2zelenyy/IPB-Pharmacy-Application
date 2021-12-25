4ae4667ed3d3f67692e06edda236b7f6612cd4f5
Ivan Zelenyy <m307924@alunos.ipb.pt>
2021-12-25 04:01:34 +0000

    Create ManageStoresPage basics

--- Pharmacy.UWP/Views/Stores/StoresPage.xaml.cs
+++ Pharmacy.UWP/Views/Stores/StoresPage.xaml.cs
@@ -1,31 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Pharmacy.UWP.ViewModels.StoresViewModel;
-using Pharmacy.Infrastructure;
-using Pharmacy.Domain;
-using Pharmacy.Domain.Models;
-using System.Threading.Tasks;
 
 namespace Pharmacy.UWP.Views.Stores
 {
     public sealed partial class StoresPage : Page
     {
         public StoresViewModel StoresViewModel { get; set; }
+        public bool manageOpened = false;
 
         public StoresPage()
-        {           
+        {
             this.InitializeComponent();
             StoresViewModel = new StoresViewModel();
         }
@@ -37,7 +23,12 @@ namespace Pharmacy.UWP.Views.Stores
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
+            this.Frame.Navigate(typeof(ManageStoresPage), null);
+        }
 
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ManageStoresPage), e);
         }
     }
 }
