34a97cbc17ab9567f4659cdb1a33e96040aacb58
Ivan Zelenyy <m307924@alunos.ipb.pt>
2022-01-03 16:53:12 +0000

    Implement Delete feature for Stores

--- Pharmacy.UWP/Views/Stores/StoresPage.xaml.cs
+++ Pharmacy.UWP/Views/Stores/StoresPage.xaml.cs
@@ -32,19 +32,30 @@ namespace Pharmacy.UWP.Views.Stores
             this.Frame.Navigate(typeof(ManageStoresPage), EditButton.Label);          
         }
 
-        private async void StoresListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void StoresListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Domain.Models.Stores selectedStore = (Domain.Models.Stores)StoresListView.SelectedItem;
-            
+
+            StoresViewModel.Id = selectedStore.Id;
             StoresViewModel.Name = selectedStore.Name;
             StoresViewModel.Country = selectedStore.Country;
             StoresViewModel.City = selectedStore.City;
             StoresViewModel.Street = selectedStore.Street;
             StoresViewModel.Telephone = selectedStore.Telephone;
-            StoresViewModel.OpeningHours = selectedStore.OpeningHours;
-
-            await StoresViewModel.DeleteStoreAsync();
+            StoresViewModel.OpeningHours = selectedStore.OpeningHours;            
         }
-      
+
+        private async void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await StoresViewModel.DeleteStoreAsync();
+            }
+            catch
+            {
+              
+            }     
+        }
+
     }
 }
