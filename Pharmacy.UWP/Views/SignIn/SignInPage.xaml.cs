e0618d3f5b966aed0a649a2ade3bcf94628a394f
Gersica <a37785@alunos.ipb.pt>
2022-01-11 13:55:01 +0000

    Create xaml.cs SignIn Page

--- Pharmacy.UWP/Views/SignIn/SignInPage.xaml.cs
+++ Pharmacy.UWP/Views/SignIn/SignInPage.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Pharmacy.UWP.ViewModels.UsersViewModel;
+using Pharmacy.UWP.Views.Menu;
+using Pharmacy.UWP.Views.Registration;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,18 +19,74 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Pharmacy.UWP.Views.SignIn
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class SignInPage : Page
     {
+        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+        public UsersViewModel UsersViewModel { get; set; }
+
         public SignInPage()
         {
             this.InitializeComponent();
+            RememberMeCheck();
+
+            UsersViewModel = new UsersViewModel();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        public void RememberMeCheck()
+        {
+            if (Convert.ToBoolean(localSettings.Values["RememberMe"]) == true)
+            {
+                CheckBox.IsChecked = true;
+                UsernameBox.Text = localSettings.Values["Username"].ToString();
+                PasswordBox.Password = localSettings.Values["Password"].ToString();
+            }
+                
+            else
+            {
+                CheckBox.IsChecked = false;
+                localSettings.Values["Username"] = "";
+                localSettings.Values["Password"] = "";
+            }               
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            localSettings.Values["RememberMe"] = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            localSettings.Values["RememberMe"] = false;
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckBox.IsChecked == true)
+            {
+                localSettings.Values["Username"] = UsernameBox.Text;
+                localSettings.Values["Password"] = PasswordBox.Password;
+            }
+
+            await UsersViewModel.GetAuthorisedUserAsync(UsernameBox.Text, PasswordBox.Password);
+
+            if (UsersViewModel.Users.Count == 1)
+            {
+                this.Frame.Navigate(typeof(MenuPage), UsersViewModel);
+            }
+            else
+                ErrorText.Visibility = Visibility.Visible;
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(RegistrationPage));
         }
     }
 }
